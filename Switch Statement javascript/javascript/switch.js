6e1f928f954bd29c12422774d8396b7cdd4a4cec
komal rana <komal.rana@zensar.com>
2020-07-23 07:21:25 +0530

    Add assignment 6

---
+++ Switch Statement javascript/javascript/switch.js
@@ -0,0 +1,19 @@
+var weekDay=new Date();
+switch (weekDay.getDay()) { 
+    case 0: console.log("Today is Sunday"); 
+    break; 
+    case 1: console.log("Today is Monday"); 
+    break; 
+    case 2: console.log("Today is Tuesday"); 
+    break; 
+    case 3: console.log("Today is Wednesday"); 
+    break; 
+    case 4: console.log("Today is Thursday"); 
+    break; 
+    case 5: console.log("Today is Friday"); 
+    break; 
+    case 6: console.log("Today is Saturday"); 
+    break; 
+    default: console.log("not equal to any given days"); 
+    break;
+}
---
+++ while loop javascript/javascript/while.js
@@ -0,0 +1,6 @@
+'use strict'
+let a = 0;
+while (a < 6) {
+  console.log(`The number is ${a}`)
+  a++;
+}
